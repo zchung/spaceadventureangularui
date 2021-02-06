6c31d44b9251fedc08cd0590d98728212e9606c1
Zane <zachung85@gmail.com>
2021-02-07 03:50:18 +1100

    updated url in prod to use heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  gameApiUrl: 'http://spaceadventurelinux.australiaeast.cloudapp.azure.com:6004/api/game/',
+  gameApiUrl: 'https://spaceadventuregameapi.herokuapp.com/api/game/',
   production: true
 };
