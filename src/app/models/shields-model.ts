78427d3a4e354c62fbe7813f414669e69c0d5707
Zane <zachung85@gmail.com>
2022-06-05 22:23:14 +1000

    added ability to save the apiKey

--- src/app/models/shields-model.ts
+++ src/app/models/shields-model.ts
@@ -2,5 +2,5 @@ import { Damageable } from './damageable';
 import { PoweredEquipment } from './powered-equipment';
 
 export interface ShieldsModel extends Damageable, PoweredEquipment {
-
+  regenAmount: number;
 }
